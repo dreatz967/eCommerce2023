e73e9ac1d6afba0eae9a4268982e4582158f34cf
dreatz967 <121359910+dreatz967@users.noreply.github.com>
2023-01-28 14:55:49 -0600

    user routes/controller/models

---
+++ ecommerce/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+//import routes
+const userRoutes = require('./routes/user.js')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+
+//middleware
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+//routes middleware
+app.use('/api', userRoutes)
+
+
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`)
+})
+
+//db connection
+mongoose.connect(
+    process.env.MONGO_URI,
+    {useNewUrlParser: true}
+  )
+  .then(() => console.log('DB Connected'))
+   
+  mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`)
+  });
+
+  
+/*mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));*/
